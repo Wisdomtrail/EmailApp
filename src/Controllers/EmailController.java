a594f86f651e246a1f1f12b730612574dde05946
sunday <117924550+sunepa01@users.noreply.github.com>
2023-03-22 23:26:42 +0100

    Add files via upload

---
+++ src/Controllers/EmailController.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+import Services.EmailServices;
+import Services.EmailServicesImpl;
+
+public class EmailController {
+
+    private final EmailServices emailServices = new EmailServicesImpl();
+
+}
---
+++ src/Services/UserServices.java
@@ -0,0 +1,13 @@
+package Services;
+
+import Dto.Request.CreateUserRequest;
+import data.models.User;
+
+public interface UserServices {
+    void register(CreateUserRequest registerRequest);
+    User findByEmail(String email);
+    int countUsers();
+    void deleteUser(int id);
+
+    String login(CreateUserRequest request);
+}

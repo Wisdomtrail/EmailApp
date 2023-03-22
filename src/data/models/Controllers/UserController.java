6cc1558475a9a5e22989229d498bfd5b89d9b366
sunday <117924550+sunepa01@users.noreply.github.com>
2023-03-22 23:25:32 +0100

    Delete src/data directory

--- src/data/models/Controllers/UserController.java
+++
@@ -1,9 +0,0 @@
-package Controllers;
-
-import Services.UserServices;
-import Services.UserServicesImpl;
-
-public class UserController {
-
-    UserServices userServices = new UserServicesImpl();
-}

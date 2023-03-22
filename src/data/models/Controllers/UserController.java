7bfde8d4af670e5e0afeb7bf09c9e351d86e4c9b
sunday <117924550+sunepa01@users.noreply.github.com>
2023-03-22 23:12:50 +0100

    Add files via upload

---
+++ src/data/models/Controllers/UserController.java
@@ -0,0 +1,9 @@
+package Controllers;
+
+import Services.UserServices;
+import Services.UserServicesImpl;
+
+public class UserController {
+
+    UserServices userServices = new UserServicesImpl();
+}

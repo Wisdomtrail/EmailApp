017302638008bd5870fc2587be53dae6049993d5
sunday <117924550+sunepa01@users.noreply.github.com>
2023-03-23 18:31:39 +0100

    Add files via upload

---
+++ src/Controllers/EmailController.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import Dto.Request.EmailRequest;
+import Services.EmailServices;
+import Services.EmailServicesImpl;
+
+public class EmailController {
+
+    private final EmailServices emailServices = new EmailServicesImpl();
+
+
+    public String sendMail(EmailRequest request){
+       return emailServices.sendMail(request);
+    }
+    public String checkMail(int id){
+       return emailServices.checkMail(id);
+    }
+    public void deleteMail(int id){
+        emailServices.deleteMail(id);
+    }
+    public int mailSize(){
+        return emailServices.mailSize();
+    }
+}
